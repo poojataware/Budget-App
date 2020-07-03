267403da293388af37aeb677ee273e330a53673c
poojataware <65983983+poojataware@users.noreply.github.com>
2020-07-03 16:10:00 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {
+    
+})();
+
+// UI CONTROLLER
+var UIcontrroller= (function(){
+    //some code
+})();
+
+// GLOBAL APP CONTROLLER
+var Controller = (function(budgetCtrl, UIcotrl)
+    {
+      document.querySelector(".add__btn").addEventListener('click',function(){
+          //Get the field input data
+
+          //Add the item to the budget controller 
+
+          //Add the item to the UI controller
+
+          // calculate the budget 
+
+          //Display the budget on the UI
+
+      });
+
+      document.addEventListener('keypress', function(event){
+          if(event.keyCode === 13 || event.which ===)
+      })
+        
+})(budgetController,UIcontrroller);
