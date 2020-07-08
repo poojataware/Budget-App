77a05642316c4c04bceaaf4169238e1098d2e93c
poojataware <65983983+poojataware@users.noreply.github.com>
2020-07-08 19:14:07 +0200

    Update app.js
    Convert field inputs to numbers, prevent false inputs.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
 
-    var Expense = function(id, description, value) {
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    
+
     };
 
-    var Income = function(id, description, value) {
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-        
+
     };
 
     var data = {
@@ -27,47 +27,53 @@ var budgetController = (function () {
     }
     return {
 
-        addItem: function(type, des, val) {
+        addItem: function (type, des, val) {
             var newItem, ID;
-            
+
             //[1 2 3 4 5], next ID = 6
             //[1 2 4 6 8], next ID = 9
             // ID = last ID + 1
-            
+
             // Create new ID
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
-            
+
             // Create new item based on 'inc' or 'exp' type
             if (type === 'exp') {
                 newItem = new Expense(ID, des, val);
             } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
-            
+
             // Push it into our data structure
             data.allItems[type].push(newItem);
             
+
             // Return the new element
             return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
         }
-        
+
     }
+
 })();
 
 
 // UI CONTROLLER
 var UIcontrroller = (function () {
     var DOMstrings = {
-        inputType:'.add__type',
-        inputDescription:'.add__description',
-        inputValue:'.add__value',
-        inputBtn:'.add__btn',
-        incomeContainer:'.income__list',
-        expensesContainer:'.expenses__list'
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
 
     };
     return {
@@ -75,44 +81,44 @@ var UIcontrroller = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                number: document.querySelector(DOMstrings.inputValue).value
+                number: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
-        
-        addListItem: function(obj, type) {
+
+        addListItem: function (obj, type) {
             var html, newHtml, element;
             // Create HTML string with placeholder text
-            
+
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                
+
                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                
+
                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            
+
             // Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
             newHtml = newHtml.replace('%value%', obj.value);
-            
+
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
-        clearFields: function(){
-            var fields,fieldsArr;
-            fields = document.querySelectorAll(DOMstrings.inputDescription+ ','+ DOMstrings.inputValue );
-        
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
             fieldsArr = Array.prototype.slice.call(fields);
 
-           fieldsArr.forEach(function(current, index, array){
-               current.value = "";
-           }); 
-           fields[0].focus();  
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
+            });
+            fields[0].focus();
         },
 
         getDomString: function () {
@@ -138,25 +144,38 @@ var Controller = (function (budgetCtrl, UIcotrl) {
         })
     }
 
-    var ctrlAddItem = function () {
-        var input, newItem;
-
-        //Get the field input data
-
-        input = UIcontrroller.getItem();
-        console.log(input);
-
-        //Add the item to the budget controller 
-        newItem = budgetCtrl.addItem(input.type, input.description, input.number);
-        //Add the item to the UI controller
-        UIcotrl.addListItem(newItem, input.type);
-        //clear the fields
-        UIcotrl.clearFields();
+    var updateBudget = function () {
 
         // calculate the budget 
 
         //Display the budget on the UI
     }
+
+    var ctrlAddItem = function () {
+        var input, newItem;
+
+        //Get the field input data
+        input = UIcontrroller.getItem();
+        console.log(input);
+
+        if (input.description !== "" && !isNaN(input.number) && input.number>0) {
+
+            //Add the item to the budget controller 
+            newItem = budgetCtrl.addItem(input.type, input.description, input.number);
+
+            //Add the item to the UI controller
+            UIcotrl.addListItem(newItem, input.type);
+
+            //clear the fields
+            UIcotrl.clearFields();
+
+
+        }
+
+
+    }
+
+
     return {
         init: function () {
             console.log('inside a function'),
